f535f28161de5b5cfb366afac82477201083e72b
Clara Madeleine Gómez Moreira <tosigether598@gmail.com>
2024-10-19 12:32:15 +0200

    Initialize project structure with dependencies and packages

---
+++ home-inventory-kotlin-app/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+n
+>app/src/main/java/com/example/homeinventoryapp/MainActivity.kt,5\a\5a918c80a7d22087a539ae87847c05283edffa52
+I
+gradle/libs.versions.toml,d\7\d7595319a0658be6f1ed500ad590d42e4b28bc4f
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+z
+Japp/src/main/java/com/example/homeinventoryapp/HomeInventoryApplication.kt,c\f\cf6bf78ed21a1b33d80329f4e4ce9037409056d9
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
---
+++ home-inventory-kotlin-app/app/src/main/java/com/example/homeinventoryapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.homeinventoryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.homeinventoryapp.ui.theme.HomeInventoryAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HomeInventoryAppTheme(
+                darkTheme = false
+            ) {
+                Surface(color = MaterialTheme.colorScheme.background){
+                    //navigation composable to be placed here
+
+                    Greeting(
+                        name = "Android",
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HomeInventoryAppTheme {
+        Greeting("Android")
+    }
+}
