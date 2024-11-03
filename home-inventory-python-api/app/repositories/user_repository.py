6f7b234a866102861d1b01c73551dae2d16b51f3
MadeleineGomezMoreira <tosigether598@gmail.com>
2024-11-03 21:30:19 +0100

    creates repository methods and pydantyic models to access users by role in a home - server

--- home-inventory-python-api/app/repositories/user_repository.py
+++ home-inventory-python-api/app/repositories/user_repository.py
@@ -1,5 +1,5 @@
-from app.models.models import User
-from app.schemas.user import UserCreate
+from app.models.models import User, UserHome
+from app.schemas.user import UserCreate, UserResponse
 from app.mappers.user_mapper import map_userCreate_to_user
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
@@ -14,6 +14,34 @@ async def save_user(session: AsyncSession, user: UserCreate):
     await session.commit()
     await session.refresh(mapped_user)
     return mapped_user
+
+
+# Get all users from a home grouped by role
+async def get_all_users_by_home_by_role(session: AsyncSession, home_id: int):
+    stmt = (
+        select(User, UserHome.role)
+        .joim(UserHome)
+        .filter(
+            UserHome.home_id == home_id,
+        )
+    )
+    result = await session.execute(stmt)
+
+    users_by_role = {"OWNER": [], "MEMBER": []}
+
+    for user, role in result.all():
+        users_by_role = UserResponse(
+            id=user.id,
+            username=user.username,
+            email=user.email,
+        )
+        users_by_role[role].append()
+
+    print(users_by_role)
+
+    return UsersByRoleResponse(
+        OWNER=users_by_role["OWNER"], MEMBER=users_by_role["MEMBER"]
+    )
 
 
 # Delete an existing user by user.id
