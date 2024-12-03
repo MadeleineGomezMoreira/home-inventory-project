9d27bd7607aff3c098db932597a098fb37f33f06
Clara Madeleine Gómez Moreira <tosigether598@gmail.com>
2024-12-03 10:49:11 +0100

    adds github actions, docker yml file and dockerfile

--- home-inventory-python-api/app/database/db_engine.py
+++ home-inventory-python-api/app/database/db_engine.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from app.database.config import DATABASE_URL
 from app.models.base import Base
 from sqlalchemy import text
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'default_value')
 
 
 engine = create_async_engine(
